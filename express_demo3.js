1eee09f9f9bcbc3cb6ef39f75dcb1090abe751d6
mrhopelee <mrhopelee@gmail.com>
2016-02-03 19:25:07 +0800

    start

---
+++ express_demo3.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Administrator on 2016-2-3.
+ */
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.send('Hello World');
+})
+
+var server = app.listen(27020, function () {
+
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("应用实例，访问地址为 http://%s:%s", host, port)
+
+})
