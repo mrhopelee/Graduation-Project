178e1cde4f3b5ca2f759cfe9c9fd4d248a9e014c
mrhopelee <mrhopelee@gmail.com>
2016-02-16 19:23:46 +0800

    看完express教程

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 /**
  * Created by Administrator on 2016-2-3.
  */
+/*在表单中通过 GET 方法提交两个参数*/
+/*
 var express = require('express');
 var app = express();
 
@@ -16,6 +18,40 @@ app.get('/process_get', function (req, res) {
     response = {
         first_name:req.query.first_name,
         last_name:req.query.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+})
+
+var server = app.listen(27021, function () {
+
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("应用实例，访问地址为 http://%s:%s", host, port)
+
+})
+*/
+/*在表单中通过 POST 方法提交两个参数*/
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+// 创建 application/x-www-form-urlencoded 编码解析
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(express.static('public'));
+
+app.get('/index.html', function (req, res) {
+    res.sendFile( __dirname + "/" + "index.html" );
+})
+
+app.post('/process_post', urlencodedParser, function (req, res) {
+
+    // 输出 JSON 格式
+    response = {
+        first_name:req.body.first_name,
+        last_name:req.body.last_name
     };
     console.log(response);
     res.end(JSON.stringify(response));
