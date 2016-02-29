6e3f34339a76614de5814c715358c48a05f028de
mrhopelee <mrhopelee@gmail.com>
2016-02-29 18:40:52 +0800

    上传

--- server.js
+++ server.js
@@ -5,28 +5,62 @@
 
 var express = require('express');
 var app = express();
+var fs = require("fs");
+
 var str =  'mongodb://' + 'localhost' +':' + '27017'+ '/' + 'gp';
 var db = require('mongoskin').db(str);
 app.set('views', './views');
 app.set('view engine', 'jade');
 
+
+var bodyParser = require('body-parser');
+var multer  = require('multer');
+
 app.use(express.static('public'));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({ dest: '/tmp/'}).array('image'));
+
 app.get('/index.html', function (req, res) {
+    console.log("index.html");
     res.sendFile( __dirname + "/" + "index.html" );
 })
 
-app.get('/picture_get_all', function (req, res) {
+app.post('/file_upload', function (req, res) {
 
-    // 输出 JSON 格式
-    db.collection('picture').find().toArray(function(err, result) {
-        if (err) throw err;
-        console.log(result);
-        res.render('manager',{picture:'1.jpg',result:result});
-        //res.send(result);
+    console.log(req.files[0]);  // 上传的文件信息
+
+    var des_file = __dirname + "/public/upload/" + req.files[0].originalname;
+    fs.readFile( req.files[0].path, function (err, data) {
+        fs.writeFile(des_file, data, function (err) {
+            if( err ){
+                console.log( err );
+            }else{
+                response = {
+                    message:'File uploaded successfully',
+                    filename:req.files[0].originalname
+                };
+            }
+            console.log( response );
+            managerpage(req, res);
+        });
     });
+})
+
+app.get('/picture_get_all', function (req, res) {
+    managerpage(req, res);
 
 })
+
+//跳转页面
+var managerpage=function (req, res) {
+    db.collection('picture').find().toArray(function(err, result) {
+        if (err) throw err;
+        //console.log(result);
+        res.render('manager',{picture:'1.jpg',result:result});
+        //res.send(result);
+    })
+}
 
 
 
